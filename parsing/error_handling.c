7d511e3a1ea7af5b90b34cad6630c1f756c0effc
Omar Chkaoulou <ochkaoul@fu-r9-p4.s19.be>
2025-10-16 12:14:12 +0200

    syntax error

--- parsing/error_handling.c
+++ parsing/error_handling.c
@@ -32,6 +32,9 @@ t_token 	*check_char(t_token *list, t_shell **all)
 		return (NULL);
 	}
 
+	// /*if cat >> then again a redir*/
+	// if(list->next->value)
+
 	return (list);
 }
 
@@ -81,7 +84,7 @@ t_token 	*check_redir(t_token *list, t_shell **all)
 	&& (list->next != NULL && list->next->type != WORD && list->next->type != PIPE))
 	{
 		write(2, "syntax error near unexpected token `", 36);
-		write(2, token[list->type - 1], ft_strlen(token[list->type - 1]));
+		write(2, token[list->next->type - 1], ft_strlen(token[list->next->type - 1]));
 		write(2, "'\n", 2);
 		(*all)->last_status = 2;
 		return NULL;
