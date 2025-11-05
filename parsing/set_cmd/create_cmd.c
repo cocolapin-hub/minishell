35fb2fe04eb84edd3dec3a404e7b0e72fda0f5bc
Omar Chkaoulou <ochkaoul@fu-r10-p3.s19.be>
2025-11-05 15:41:29 +0100

    ha

--- parsing/set_cmd/create_cmd.c
+++ parsing/set_cmd/create_cmd.c
@@ -6,37 +6,108 @@
 /*   By: ochkaoul <ochkaoul@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 11:41:36 by ochkaoul          #+#    #+#             */
-/*   Updated: 2025/11/05 10:59:55 by ochkaoul         ###   ########.fr       */
+/*   Updated: 2025/11/05 11:09:44 by ochkaoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	create_cmd(t_token **tmp, t_token **new, t_token **start, t_token **end)
-{
-	t_token	*target;
 
-	target = (*tmp)->next;
-	while (target && target->type == WORD && target->value[0] == '-')
-		target = target->next;
-	if (target && target->type == WORD)
-	{
-		*new = malloc(sizeof(t_token));
-		(*new)->type = (*tmp)->type;
-		(*new)->value = ft_strdup(target->value);
-		if (!(*new)->value)
-		{
-			free(*new);
-			*new = NULL;
-			return ;
-		}
-		(*new)->amount = target->amount;
-		(*new)->next = NULL;
-		if (!(*start))
-			*start = *new;
-		else
-			(*end)->next = *new;
-		*end = *new;
-		*tmp = target;
-	}
+
+
+
+static t_token *find_target(t_token *tmp)
+{
+    t_token *target;
+
+    target = tmp->next;
+    while (target && target->type == WORD && target->value[0] == '-')
+        target = target->next;
+    if (target && target->type == WORD)
+        return (target);
+    return (NULL);
 }
+
+// static t_token *create_token_from(t_token *src)
+// {
+//     t_token *new;
+
+//     new = malloc(sizeof(t_token));
+//     if (!new)
+//         return (NULL);
+//     new->type = src->type;
+//     new->value = ft_strdup(src->value);
+//     if (!new->value)
+//     {
+//         free(new);
+//         return (NULL);
+//     }
+//     new->amount = src->amount;
+//     new->heredoc_fd = -1;  // Initialize this!
+//     new->next = NULL;
+//     return (new);
+// }
+
+void create_cmd(t_token **tmp, t_token **new, t_token **start, t_token **end)
+{
+    t_token *target;
+
+    target = find_target(*tmp);
+    if (!target)
+        return;
+
+    // KEY FIX: We need to copy the REDIRECTION token, not the target WORD token!
+    *new = malloc(sizeof(t_token));
+    if (!*new)
+        return;
+
+    (*new)->type = (*tmp)->type;  // Use the redirection type (REDIR_IN, etc.)
+    (*new)->value = ft_strdup(target->value);  // But copy the target's value
+    if (!(*new)->value)
+    {
+        free(*new);
+        *new = NULL;
+        return;
+    }
+    (*new)->amount = target->amount;
+    (*new)->heredoc_fd = -1;  // Initialize this!
+    (*new)->next = NULL;
+
+    if (!*start)
+        *start = *new;
+    else
+        (*end)->next = *new;
+    *end = *new;
+    *tmp = target;
+}
+
+
+
+// void	create_cmd(t_token **tmp, t_token **new, t_token **start, t_token **end)
+// {
+// 	t_token	*target;
+
+// 	target = (*tmp)->next;
+// 	while (target && target->type == WORD && target->value[0] == '-')
+// 		target = target->next;
+// 	if (target && target->type == WORD)
+// 	{
+// 		*new = malloc(sizeof(t_token));
+// 		(*new)->type = (*tmp)->type;
+// 		(*new)->value = ft_strdup(target->value);
+// 		if (!(*new)->value)
+// 		{
+// 			free(*new);
+// 			*new = NULL;
+// 			return ;
+// 		}
+// 		(*new)->amount = target->amount;
+// 		(*new)->next = NULL;
+// 		if (!(*start))
+// 			*start = *new;
+// 		else
+// 			(*end)->next = *new;
+// 		*end = *new;
+// 		*tmp = target;
+// 	}
+// }
